99890176e724083c10a0c05852333669a42c8908
HristovCodes <ivailohristov1990@gmail.com>
2021-04-10 15:22:20 +0300

    Removed "whitelist" and cleaned up code

--- commands/afk.js
+++ commands/afk.js
@@ -17,18 +17,15 @@ module.exports = {
   permissions: "",
   description: "Going on vacation?",
   execute(message, args) {
-    firebase.cleanupDates();
+    firebase.cleanupDates(message.guild.id);
     // When someone uses the bot I'll see what they did for easier debugging
     console.log(
       `${message.member.user.tag} used the bot.\nDate: ${message.createdAt}.\nMessage: ${message.content}\n---------------`
     );
 
-    if (
-      message.channel.id !== "804718851903848448" &&
-      message.channel.id !== "805423509174091778"
-    ) {
+    if (!message.member.hasPermission("MENTION_EVERYONE")) {
       return message.channel.send(
-        `You cannot use \$${this.name} here or you are lacking the permission to do so. ${message.author}`
+        `You cannot use \$${this.name} here because you are lacking the permission to do so. ${message.author}`
       );
     }
     if (!args[0]) {
@@ -63,23 +60,19 @@ module.exports = {
             );
 
             if (botMessage) {
-              let content = botMessage.content;
-              if (content.includes(args[0])) message.delete();
-              else {
-                // if the user is not in the pinned message add him to the database and to the pinned message
-                firebase.addAfk(
-                  message.guild.id,
-                  id,
-                  calculateDate(args[1]),
-                  args[1]
-                );
+              // if the user is not in the pinned message add him to the database and to the pinned message
+              firebase.addAfk(
+                message.guild.id,
+                id,
+                calculateDate(args[1]),
+                args[1]
+              );
 
-                botMessage.edit(
-                  `Inactive people:\n${args[0]} for ${
-                    args[1]
-                  } weeks | ${calculateDate(args[1])}\n${data.join("\n")}`
-                );
-              }
+              botMessage.edit(
+                `Inactive people:\n${args[0]} for ${
+                  args[1]
+                } weeks | ${calculateDate(args[1])}\n${data.join("\n")}`
+              );
             } else {
               // if theres no message add the user to the database and make a message and pin it
               firebase.addAfk(
